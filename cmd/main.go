f8b5276a8aea8eccc3ae8c29964dc500c6ae30b4
Laws24 <1484502768@qq.com>
2023-12-18 07:33:48 +0800

    feat:User mod

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"go-app/pkg/user"
+	"go-app/pkg/router"
 
 	"github.com/BoyYangZai/go-server-lib/pkg/config_reader"
 	"github.com/BoyYangZai/go-server-lib/pkg/database"
@@ -15,6 +15,5 @@ func main() {
 	database.InitDatabase(database.DatabaseConfig{User: config_reader.GetConfigByKey("database.User"), Password: config_reader.GetConfigByKey("database.Password"),
 		Host: config_reader.GetConfigByKey("database.Host"), Port: config_reader.GetConfigByKey("database.Port"), DBName: config_reader.GetConfigByKey("database.DBName")})
 
-	user.Run()
-
+	router.CreateRouter()
 }
